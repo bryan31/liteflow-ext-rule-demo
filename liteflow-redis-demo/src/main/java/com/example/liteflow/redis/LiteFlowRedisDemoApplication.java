a7da8159f13d17e12293125015edd5136c3ea598
houxinyu <hxinyu2000@163.com>
2023-08-24 20:32:19 +0800

    添加Redis demo

---
+++ liteflow-redis-demo/src/main/java/com/example/liteflow/redis/LiteFlowRedisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.liteflow.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteFlowRedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteFlowRedisDemoApplication.class, args);
+    }
+}
