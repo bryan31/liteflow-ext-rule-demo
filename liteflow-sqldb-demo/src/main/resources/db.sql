bb754548ed62255458af9af2933f95fcd5b6a321
everywhere.z <weenyc31@163.com>
2023-12-25 13:34:46 +0800

    commit

--- liteflow-sqldb-demo/src/main/resources/db.sql
+++ liteflow-sqldb-demo/src/main/resources/db.sql
@@ -6,7 +6,8 @@ create table chain
     chain_name       varchar(32) null,
     chain_desc       varchar(64) null,
     el_data          text        null,
-    create_time      datetime    null
+    create_time      datetime    null,
+    enable           tinyint(1)  1
 );
 
 
@@ -18,12 +19,14 @@ create table script
     script_id        varchar(32) null,
     script_name      varchar(64) null,
     script_data      text        null,
-    script_type      varchar(16) null
+    script_type      varchar(16) null,
+    language         varchar(32) null,
+    enable           tinyint(1)  1
 );
 
-INSERT INTO chain (id, application_name, chain_name, chain_desc, el_data, create_time) VALUES (1, 'demo', 'chain1', '测试流程', 'THEN(a, b, c, s1);', '2022-09-19 19:31:00');
+INSERT INTO chain (id, application_name, chain_name, chain_desc, el_data, create_time, enable) VALUES (1, 'demo', 'chain1', '测试流程', 'THEN(a, b, c, s1);', '2022-09-19 19:31:00', 1);
 
-INSERT INTO script (id, application_name, script_id, script_name, script_data, script_type) VALUES (1, 'demo', 's1', '脚本s1', 'import cn.hutool.core.date.DateUtil
+INSERT INTO script (id, application_name, script_id, script_name, script_data, script_type, language, enable) VALUES (1, 'demo', 's1', '脚本s1', 'import cn.hutool.core.date.DateUtil
 
 def date = DateUtil.parse("2022-10-17 13:31:43")
 println(date)
@@ -41,6 +44,6 @@ defaultContext.setData("student",student)
 
 def a=3
 def b=2
-defaultContext.setData("s1",a*b)', 'script');
-INSERT INTO script (id, application_name, script_id, script_name, script_data, script_type) VALUES (2, 'demo', 's2', '脚本s2', 'defaultContext.setData("s2","hello")', 'script');
+defaultContext.setData("s1",a*b)', 'script', 'groovy', 1);
+INSERT INTO script (id, application_name, script_id, script_name, script_data, script_type, language, enable) VALUES (2, 'demo', 's2', '脚本s2', 'defaultContext.setData("s2","hello")', 'script', 'groovy', 1);
 
